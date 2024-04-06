e3268afeab1d316d4521f4cd217762bc91979ff8
Tima <kulkukumber@yandex.ru>
2024-04-06 20:50:55 +0300

    Отзывы. Пока что без тестов, пусть лежат

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -0,0 +1,72 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.service.review.ReviewService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/reviews")
+@AllArgsConstructor
+@Slf4j
+public class ReviewController {
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public Review createReview(@RequestBody Review review) {
+        log.info("createReview, id = " + review.getReviewId());
+        return reviewService.create(review);
+    }
+
+    @PutMapping
+    public Review updateReview(@RequestBody Review review) {
+        log.info("updateReview, id = " + review.getReviewId());
+        return reviewService.update(review);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReview(@PathVariable Integer id) {
+        log.info("deleteReview, id = " + id);
+        reviewService.deleteReview(id);
+    }
+
+    @GetMapping("/{id}")
+    public Review findReview(@PathVariable Integer id) {
+        log.info("findReview, id = " + id);
+        return reviewService.get(id);
+    }
+
+    @GetMapping
+    public Collection<Review> getReviews(@RequestParam(required = false) Integer filmId,
+                                         @RequestParam(defaultValue = "10") Integer count) {
+        log.info("getReviews, filmId = " + filmId + ", count = " + count);
+        return reviewService.getAll(filmId, count);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void likeReview(@PathVariable Integer id, @PathVariable Integer userId) {
+        log.info("likeReview, id = " + id + "userId = " + userId);
+        reviewService.putLike(id, userId);
+    }
+
+    @PutMapping("/{id}/dislike/{userId}")
+    public void dislikeReview(@PathVariable Integer id, @PathVariable Integer userId) {
+        log.info("dislikeReview, id = " + id + "userId = " + userId);
+        reviewService.putDislike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void deleteLikeReview(@PathVariable Integer id, @PathVariable Integer userId) {
+        log.info("deleteLikeReview, id = " + id + "userId = " + userId);
+        reviewService.deleteLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/dislike/{userId}")
+    public void deleteDislikeReview(@PathVariable Integer id, @PathVariable Integer userId) {
+        log.info("deleteDislikeReview, id = " + id + "userId = " + userId);
+        reviewService.deleteDislike(id, userId);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.storage.review;
+
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.Collection;
+
+public interface ReviewStorage {
+    Review create(Review review);
+    Review update(Review review);
+    Review get(Integer id);
+    Collection<Review> getAll(Integer filmId, Integer count);
+    void deleteReview(Integer id);
+    void putLike(Integer id, Integer userId);
+    void putDislike(Integer id, Integer userId);
+    void deleteLike(Integer id, Integer userId);
+    void deleteDislike(Integer id, Integer userId);
+}
