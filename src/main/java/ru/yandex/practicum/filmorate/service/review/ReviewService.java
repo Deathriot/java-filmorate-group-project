f318ce6af3e26760a5d175b3deef2e76ac0bb7bf
Tima <kulkukumber@yandex.ru>
2024-04-06 20:50:55 +0300

    Feat: add-reviews. Ребейз режиссеров и ивентов прошел успешно

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/review/ReviewService.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.service.review;
+
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.Collection;
+
+public interface ReviewService {
+    Review create(Review review);
+    Review update(Review review);
+    Review get(Integer id);
+    Collection<Review> getAll(Integer filmId, Integer count);
+    void deleteReview(Integer id);
+    void putLike(Integer id, Integer userId);
+    void putDislike(Integer id, Integer userId);
+    void deleteLike(Integer id, Integer userId);
+    void deleteDislike(Integer id, Integer userId);
+
+}
